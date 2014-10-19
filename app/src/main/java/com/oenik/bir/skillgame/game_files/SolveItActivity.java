2558e0a26e8b7580aafd22ed79ddaf787b8df047
Roland Matyóka <mroland91@gmail.com>
2014-10-19 22:00:53 +0200

    SolveIt java class changes
    Changes:
    -minor code optimization

--- app/src/main/java/com/oenik/bir/skillgame/game_files/SolveItActivity.java
+++ app/src/main/java/com/oenik/bir/skillgame/game_files/SolveItActivity.java
@@ -14,12 +14,28 @@ import java.util.Random;
 
 public class SolveItActivity extends Activity {
 
+    View.OnClickListener onClickListenerFailed = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            TextView score = (TextView) findViewById(R.id.solve_it_score_label);
+            score.setText("FAILED");
+            //next game
+        }
+    };
+
+    View.OnClickListener onClickListenerPassed = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            TextView score = (TextView) findViewById(R.id.solve_it_score_label);
+            score.setText("1");
+        }
+    };
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_solve_it);
 
-        Toast.makeText(this, "Oldd meg az egyenletet!", Toast.LENGTH_LONG).show();
 
         String[] equations = new String[] {"5*5+10=35", "56/7+6=14", "(91-27)/8=8"};
         Random r = new Random();
@@ -27,7 +43,7 @@ public class SolveItActivity extends Activity {
         char key = selected.charAt(r.nextInt(selected.length()));
 
         TextView tv = (TextView) findViewById(R.id.equation_label);
-        selected = selected.replaceFirst(Character.toString(key),"?");
+        selected = selected.replace(key, '?');
         tv.setText(selected);
 
         int solution = r.nextInt(3);
@@ -37,106 +53,43 @@ public class SolveItActivity extends Activity {
                 //set solution answer for #1
                 Button button1_1 = (Button) findViewById(R.id.first_answer);
                 button1_1.setText(String.valueOf(key));
-                button1_1.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View view) {
-                        TextView score = (TextView) findViewById(R.id.solve_it_score_label);
-                        score.setText("1");
-                        //next game
-                    }
-                });
+                button1_1.setOnClickListener(onClickListenerPassed);
                 //set other answer for #2
                 Button button2_1 = (Button) findViewById(R.id.second_answer);
                 button2_1.setText("a");
-                button2_1.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View view) {
-                        TextView score = (TextView) findViewById(R.id.solve_it_score_label);
-                        score.setText("FAILED");
-                        //next game
-                    }
-                });
+                button2_1.setOnClickListener(onClickListenerFailed);
                 //set other answer for #3
                 Button button3_1 = (Button) findViewById(R.id.third_answer);
                 button3_1.setText("b");
-                button3_1.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View view) {
-                        TextView score = (TextView) findViewById(R.id.solve_it_score_label);
-                        score.setText("FAILED");
-                        //next game
-                    }
-                });
+                button3_1.setOnClickListener(onClickListenerFailed);
                 break;
             case 1:
                 //set solution answer for #2
                 Button button2_2 = (Button) findViewById(R.id.second_answer);
                 button2_2.setText(String.valueOf(key));
-                button2_2.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View view) {
-                        TextView score = (TextView) findViewById(R.id.solve_it_score_label);
-                        score.setText("1");
-                        //next game
-                    }
-                });
+                button2_2.setOnClickListener(onClickListenerPassed);
                 //set other answer for #1
                 Button button1_2 = (Button) findViewById(R.id.first_answer);
                 button1_2.setText("a");
-                button1_2.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View view) {
-                        TextView score = (TextView) findViewById(R.id.solve_it_score_label);
-                        score.setText("FAILED");
-                        //next game
-                    }
-                });
+                button1_2.setOnClickListener(onClickListenerFailed);
                 //set other answer for #3
                 Button button3_2 = (Button) findViewById(R.id.third_answer);
                 button3_2.setText("b");
-                button3_2.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View view) {
-                        TextView score = (TextView) findViewById(R.id.solve_it_score_label);
-                        score.setText("FAILED");
-                        //next game
-                    }
-                });
+                button3_2.setOnClickListener(onClickListenerFailed);
                 break;
             case 2:
                 //set solution answer for #3
                 Button button3_3 = (Button) findViewById(R.id.third_answer);
                 button3_3.setText(String.valueOf(key));
-                button3_3.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View view) {
-                        TextView score = (TextView) findViewById(R.id.solve_it_score_label);
-                        score.setText("1");
-                        //next game
-                    }
-                });
+                button3_3.setOnClickListener(onClickListenerPassed);
                 //set other answer for #1
                 Button button1_3 = (Button) findViewById(R.id.second_answer);
                 button1_3.setText("a");
-                button1_3.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View view) {
-                        TextView score = (TextView) findViewById(R.id.solve_it_score_label);
-                        score.setText("FAILED");
-                        //next game
-                    }
-                });
+                button1_3.setOnClickListener(onClickListenerFailed);
                 //set other answer for #2
                 Button button2_3 = (Button) findViewById(R.id.first_answer);
                 button2_3.setText("b");
-                button2_3.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View view) {
-                        TextView score = (TextView) findViewById(R.id.solve_it_score_label);
-                        score.setText("FAILED");
-                        //next game
-                    }
-                });
+                button2_3.setOnClickListener(onClickListenerFailed);
                 break;
         }
     }
