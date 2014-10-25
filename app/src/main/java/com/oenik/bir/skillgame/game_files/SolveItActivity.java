a4edcb9cb1ea826790835cccda4ee7fa8dd848d5
Roland Matyóka <mroland91@gmail.com>
2014-10-26 00:55:31 +0200

    SolveIt View
    SolveIt extends GameAbstract

--- app/src/main/java/com/oenik/bir/skillgame/game_files/SolveItActivity.java
+++ app/src/main/java/com/oenik/bir/skillgame/game_files/SolveItActivity.java
@@ -1,26 +1,48 @@
 package com.oenik.bir.skillgame.game_files;
 
 import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.TextView;
 import android.widget.Toast;
-
 import com.oenik.bir.skillgame.R;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Random;
 
 
 public class SolveItActivity extends Activity {
+    private Button first;
+    private Button second;
+    private Button third;
+
+    public static void NextGame(Context context)
+    {
+        Intent intent = new Intent(context, SolveItActivity.class);
+        context.startActivity(intent);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_solve_it);
+        first = (Button) findViewById(R.id.first_answer);
+        second = (Button) findViewById(R.id.second_answer);
+        third = (Button) findViewById(R.id.third_answer);
+        SharedPreferences sPrefs = getSharedPreferences("solveItButtons", Context.MODE_PRIVATE);
+        first.setText(sPrefs.getString("firstText", ""));
+        first.setOnClickListener(sPrefs.getString("firstListener", "") == "onClickListenerPassed" ? onClickListenerPassed : onClickListenerFailed);
+        second.setText(sPrefs.getString("secondText", ""));
+        second.setOnClickListener(sPrefs.getString("secondListener", "") == "onClickListenerPassed" ? onClickListenerPassed : onClickListenerFailed);
+        third.setText(sPrefs.getString("thirdText", ""));
+        third.setOnClickListener(sPrefs.getString("thirdListener", "") == "onClickListenerPassed" ? onClickListenerPassed : onClickListenerFailed);
+    }
 
     View.OnClickListener onClickListenerFailed = new View.OnClickListener() {
         @Override
         public void onClick(View view) {
-            TextView score = (TextView) findViewById(R.id.solve_it_score_label);
-            score.setText("FAILED");
+            Toast.makeText(getBaseContext(), "FAILED!", Toast.LENGTH_LONG).show();
             //next game
         }
     };
@@ -28,94 +50,9 @@ public class SolveItActivity extends Activity {
     View.OnClickListener onClickListenerPassed = new View.OnClickListener() {
         @Override
         public void onClick(View view) {
-            TextView score = (TextView) findViewById(R.id.solve_it_score_label);
-            score.setText("1");
+            Toast.makeText(getBaseContext(), "PASSED!", Toast.LENGTH_LONG).show();
+            //next game
         }
     };
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_solve_it);
-
-        Toast.makeText(this, "Oldd meg az egyenletet!", Toast.LENGTH_LONG).show();
-
-        //equation
-        String[] equations = new String[] {"5*5+10=35", "56/7+6=14", "(91-27)/8=8"};
-        Random r = new Random();
-        String selected = equations[r.nextInt(3)];
-        char key = selected.charAt(r.nextInt(selected.length()));
-
-        //bad answers
-        List<String> badAnswers = new ArrayList<String>();
-        badAnswers.add("*");
-        badAnswers.add("/");
-        badAnswers.add("+");
-        badAnswers.add("-");
-        badAnswers.add("(");
-        badAnswers.add(")");
-        for (int i=0; i<10; i++){
-            badAnswers.add(String.valueOf(i));
-        }
-        badAnswers.remove(String.valueOf(key));
-
-        //view
-        TextView tv = (TextView) findViewById(R.id.equation_label);
-        selected = selected.replaceFirst(Character.toString(key),"?");
-        tv.setText(selected);
-
-        //generating randoms
-        int solution = r.nextInt(3);
-        int badAnswerNumber1 = r.nextInt(badAnswers.size());
-        String firstBadAnswer = badAnswers.get(badAnswerNumber1);
-        badAnswers.remove(badAnswers.get(badAnswerNumber1));
-        int badAnswerNumber2 = r.nextInt(badAnswers.size());
-        String secondBadAnswer = badAnswers.get(badAnswerNumber2);
-
-        switch(solution) {
-            case 0:
-                //set solution answer for #1
-                Button button1_1 = (Button) findViewById(R.id.first_answer);
-                button1_1.setText(String.valueOf(key));
-                button1_1.setOnClickListener(onClickListenerPassed);
-                //set other answer for #2
-                Button button2_1 = (Button) findViewById(R.id.second_answer);
-                button2_1.setText(firstBadAnswer);
-                button2_1.setOnClickListener(onClickListenerFailed);
-                //set other answer for #3
-                Button button3_1 = (Button) findViewById(R.id.third_answer);
-                button3_1.setText(secondBadAnswer);
-                button3_1.setOnClickListener(onClickListenerFailed);
-                break;
-            case 1:
-                //set solution answer for #2
-                Button button2_2 = (Button) findViewById(R.id.second_answer);
-                button2_2.setText(String.valueOf(key));
-                button2_2.setOnClickListener(onClickListenerPassed);
-                //set other answer for #1
-                Button button1_2 = (Button) findViewById(R.id.first_answer);
-                button1_2.setText(firstBadAnswer);
-                button1_2.setOnClickListener(onClickListenerFailed);
-                //set other answer for #3
-                Button button3_2 = (Button) findViewById(R.id.third_answer);
-                button3_2.setText(secondBadAnswer);
-                button3_2.setOnClickListener(onClickListenerFailed);
-                break;
-            case 2:
-                //set solution answer for #3
-                Button button3_3 = (Button) findViewById(R.id.third_answer);
-                button3_3.setText(String.valueOf(key));
-                button3_3.setOnClickListener(onClickListenerPassed);
-                //set other answer for #1
-                Button button1_3 = (Button) findViewById(R.id.second_answer);
-                button1_3.setText(firstBadAnswer);
-                button1_3.setOnClickListener(onClickListenerFailed);
-                //set other answer for #2
-                Button button2_3 = (Button) findViewById(R.id.first_answer);
-                button2_3.setText(secondBadAnswer);
-                button2_3.setOnClickListener(onClickListenerFailed);
-                break;
-        }
-    }
 
 }
---
+++ app/src/main/java/com/oenik/bir/skillgame/game_files/SolveItView.java
@@ -0,0 +1,194 @@
+package com.oenik.bir.skillgame.game_files;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.util.AttributeSet;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.regex.Pattern;
+
+public class SolveItView extends GameAbstract{
+
+    private final String[] equations = new String[] {"5*5+10=35", "56/7+6=14", "(91-27)/8=8"};
+    private final List<Character> badAnswers;
+    private String selected;
+    private String question;
+
+    static int selectedNumber;
+    static char key;
+    static char firstBadAnswer;
+    static char secondBadAnswer;
+
+    private int score;
+    private SharedPreferences sPrefs;
+
+    private static Random r = new Random();
+    Paint paint;
+    private Context context;
+
+    public SolveItView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        badAnswers = new ArrayList<Character>();
+        paint = new Paint();
+        this.context = context;
+        sPrefs = context.getSharedPreferences("solveItButtons", Context.MODE_PRIVATE);
+        Init();
+    }
+
+//    public static boolean setInitParameters(String line) {
+//
+//        String[] tokens = line.split(":");
+//
+//        if (!tokens[0].equals(GameAbstract.SZAMOLGATO_NAME))
+//            return false;
+//
+//        code_indices = new int[GAME_COUNT];
+//        name_indices = new int[GAME_COUNT];
+//
+//        int index = 0;
+//        int length = tokens.length;
+//        for (int i=1;i<length;i+=2)
+//        {
+//            if (i < (GAME_COUNT << 1))
+//            {
+//                code_indices[index] = Integer.parseInt(tokens[i]);
+//                name_indices[index++] = Integer.parseInt(tokens[i+1]);
+//            }
+//        }
+//
+//        return true;
+//    }
+
+    public static String getGameInitString() {
+
+        StringBuilder builder = new StringBuilder();
+        builder.append(GameAbstract.SZAMOLGATO_NAME).append(":");
+        for (int i=0;i<GAME_COUNT;i++)
+        {
+            //GameInit();
+            builder.append(selectedNumber).append(":").append(key).append(":").append(firstBadAnswer).append(":").append(secondBadAnswer).append(":");
+        }
+
+        return builder.toString();
+    }
+
+    @Override
+    public void GetResult() {
+
+    }
+
+    @Override
+    public void Init() {
+        //generating bad answers
+        badAnswers.add('*');
+        badAnswers.add('/');
+        badAnswers.add('+');
+        badAnswers.add('-');
+        badAnswers.add('(');
+        badAnswers.add(')');
+        for (int i=0; i<10; i++){
+            badAnswers.add(Character.forDigit(i,10));
+        }
+        badAnswers.remove(Character.valueOf(key));
+
+
+        old_time = System.currentTimeMillis();
+        time_thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                do {
+                    long current_time = System.currentTimeMillis();
+
+                    if ((current_time - old_time) >= GAME_MILLIS) {
+                        current_game++;
+                        old_time = current_time;
+                        GameInit();
+                    }
+
+                } while (current_game < GAME_COUNT);
+
+                SolveItActivity.NextGame(context);
+
+            }
+        });
+
+        GameInit();
+
+        time_thread.start();
+    }
+
+    @Override
+    protected void GameInit() {
+        //equation and solution selection
+        selectedNumber = r.nextInt(equations.length);
+        selected = equations[selectedNumber];
+        key = selected.charAt(r.nextInt(selected.length()));
+        question = selected.replaceFirst(Pattern.quote(Character.toString(key)), "?");
+
+        //generating other answers
+        int badAnswerNumber1 = r.nextInt(badAnswers.size());
+        firstBadAnswer = badAnswers.get(badAnswerNumber1);
+        badAnswers.remove(badAnswers.get(badAnswerNumber1));
+        int badAnswerNumber2 = r.nextInt(badAnswers.size());
+        secondBadAnswer = badAnswers.get(badAnswerNumber2);
+
+        //random order
+        int solution = r.nextInt(3);
+        switch(solution) {
+            case 0:
+                //set solution answer for #1
+                sPrefs.edit().putString("firstText",String.valueOf(key)).apply();
+                sPrefs.edit().putString("firstListener","onClickListenerPassed").apply();
+//                //set other answer for #2
+                sPrefs.edit().putString("secondText",String.valueOf(firstBadAnswer)).apply();
+                sPrefs.edit().putString("secondListener","onClickListenerFailed").apply();
+//                //set other answer for #3
+                sPrefs.edit().putString("thirdText",String.valueOf(secondBadAnswer)).apply();
+                sPrefs.edit().putString("thirdListener","onClickListenerFailed").apply();
+                break;
+            case 1:
+//                //set solution answer for #2
+                sPrefs.edit().putString("secondText",String.valueOf(key)).apply();
+                sPrefs.edit().putString("secondListener","onClickListenerPassed").apply();
+//                //set other answer for #1
+                sPrefs.edit().putString("firstText",String.valueOf(firstBadAnswer)).apply();
+                sPrefs.edit().putString("firstListener","onClickListenerFailed").apply();
+//                //set other answer for #3
+                sPrefs.edit().putString("thirdText",String.valueOf(secondBadAnswer)).apply();
+                sPrefs.edit().putString("thirdListener","onClickListenerFailed").apply();
+                break;
+            case 2:
+//                //set solution answer for #3
+                sPrefs.edit().putString("thirdText",String.valueOf(key)).apply();
+                sPrefs.edit().putString("thirdListener","onClickListenerPassed").apply();
+//                //set other answer for #1
+                sPrefs.edit().putString("firstText",String.valueOf(firstBadAnswer)).apply();
+                sPrefs.edit().putString("firstListener","onClickListenerFailed").apply();
+//                //set other answer for #2
+                sPrefs.edit().putString("secondText",String.valueOf(secondBadAnswer)).apply();
+                sPrefs.edit().putString("secondListener","onClickListenerFailed").apply();
+                break;
+        }
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+
+        int width = getWidth();
+        int height = getHeight();
+
+        paint.setColor(Color.BLACK);
+        paint.setTextSize(18);
+        paint.setTextAlign(Paint.Align.LEFT);
+        canvas.drawText("Score: "+Integer.toString(score), 0, 0+paint.getTextSize(), paint);
+
+        paint.setTextAlign(Paint.Align.CENTER);
+        canvas.drawText(question, width/2, height/4, paint);
+    }
+}
